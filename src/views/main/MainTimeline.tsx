091fad4ad6cd81ead640da8111f08cea6ef63e10
deyt <deyt.it02@gmail.com>
2023-12-22 23:56:41 +0900

    [add] 타임라인 기능구현 20%

--- src/views/main/MainTimeline.tsx
+++ src/views/main/MainTimeline.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef, useState } from 'react'
+
 export default function MainTimeline(){
+	const startYear = 2019;
+	const endYear = new Date().getFullYear()
+
+	// {현 scrollTop값}이 {특정section 위치 - viewport높이값 1/3}에 오면
+	// ㄴ 그 section의 {period}에 해당하는 li에 class on
+	const onScroll = () => {
+		const scrollTop = window.scrollY;
+		// console.log(scrollTop);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', onScroll, { passive: true });
+
+		return () => {
+			window.removeEventListener('scroll', onScroll);
+		};
+	}, []);
+	
 	return (
 		<ul className='timeline'>
-			<li className='off'>NOW</li>
+			{
+				Array.from({length: endYear-startYear+1}, (_, i)=>endYear-i).map(year =>
+					<li data-year={year}>{year === endYear ? 'NOW' : year}</li>
+				)
+			}
+			{/* <li data-year="2023" className='off'>NOW</li>
 			<li>2022</li>
 			<li>2021</li>
 			<li>2020</li>
-			<li className='off'>2019</li>
+			<li className='off'>2019</li> */}
 		</ul>
 	)
 }
--- src/views/main/MainView.tsx
+++ src/views/main/MainView.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { careerItems } from './MainCareerItems';
 import Slider from '../../components/slider/Slider';
 import MainProjectDesc from './MainProjectDesc';
@@ -6,29 +7,50 @@ import '../../assets/styles/main.scss';
 
 
 export default function MainView(){
+	const projectRefs = useRef([])
+
+	// ing..
+	// refs 모두 업뎃되면, props 뽑아서 timeline에 전달
+	// ㄴ 모두 업뎃된 시점에 timeline 렌더링 실행
+
 	return (
 		<div className="main-container">
+			<h2>
+				타이틀입니다.<br />
+				두 줄이에요
+			</h2>
 			<article className='personal-info'>
-				<h2>
-					타이틀입니다.<br />
-					두 줄이에요
-				</h2>
 				<div style={{height: '275px', backgroundColor: 'pink'}}>tmp</div>
 			</article>
 
+			<h2>Career</h2>
 			<article className="career">
-				<h2>Career</h2>
+				<div className="projects">
 				{
-					careerItems.map((item, i) => 
-						<section key={i}>
-							<Slider {...item.sliderProps} containerClass="slider-area" />
-							<MainProjectDesc {...item.projectDescProps} containerClass='project-desc-area' />
-						</section>
-					)
+					careerItems.map((item, i) => {
+						const period = item.projectDescProps.period
+						const dataPeriod = period.start.getFullYear()+'-'+period.end.getFullYear()
+						
+						return (
+							<section key={i} data-period={dataPeriod} ref={projectRefs.current[i]}>
+								<Slider {...item.sliderProps} containerClass="slider-area" />
+								<MainProjectDesc {...item.projectDescProps} containerClass='project-desc-area' />
+							</section>
+						)
+					})
 				}
+				</div>
 
 				<div className="timeline-rail">
 				<div className="timeline-box">
+					{/* prop - 
+						[
+							{
+								period: {start: 2021, end: 2022}, 
+								offsetTop: 534, 
+							}, ...
+						]
+					 */}
 					<MainTimeline />
 				</div>
 				</div>
