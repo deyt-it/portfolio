31d4c2c53379aa9c4f5715b105adadc231c1c42d
deyt <deyt.it02@gmail.com>
2023-12-30 23:58:36 +0900

    [add] 타임라인 기능구현 80% - 클릭이벤트 완료, 슬라이더 아이템 추가

--- src/views/main/MainProjects.tsx
+++ src/views/main/MainProjects.tsx
@@ -3,14 +3,14 @@ import { IMainProjectDesc } from './MainProjectDesc';
 import imgTmp from '../../assets/images/img-tmp.jpg';
 import imgTmpGif from '../../assets/images/img-tmp-gif.gif';
 
-interface ICareerItem {
+interface IMainProject {
 	sliderProps: ISliderProps, 
 	projectDescProps: IMainProjectDesc, 
 }
 const slideWidth = '545px';
 const slideHeight = '330px';
 
-export const projects: Array<ICareerItem> = [
+export const projects: Array<IMainProject> = [
 	{
 		sliderProps: {
 			items: [
@@ -30,7 +30,7 @@ export const projects: Array<ICareerItem> = [
 			details: [
 				{
 					title: '포지션', 
-					content: '웹 프론트엔드', 
+					content: '웹 프론트엔드 개발자', 
 				}, 
 				{
 					title: '프로젝트', 
@@ -38,13 +38,14 @@ export const projects: Array<ICareerItem> = [
 				}, 
 				{
 					title: '담당기능', 
-					content: 'ARS설정, 회원가입, 채팅', 
+					content: '사내 네트워크(서버-시스템-서비스) 노드별 조회 및 전체조회 기능', 
 				}, 
 			], 
 			corp: '팀스톤', 
-			link: 'https://say015.com', 
+			// link: 'https://say015.com', 
 		}
 	}, 
+
 	{
 		sliderProps: {
 			items: [
@@ -67,7 +68,7 @@ export const projects: Array<ICareerItem> = [
 			details: [
 				{
 					title: '포지션', 
-					content: '웹 프론트엔드', 
+					content: '웹 프론트엔드 개발자', 
 				}, 
 				{
 					title: '프로젝트', 
@@ -75,11 +76,83 @@ export const projects: Array<ICareerItem> = [
 				}, 
 				{
 					title: '담당기능', 
-					content: 'ARS설정 v1, ARS설정 v2, 회원가입, 채팅, 관리자', 
+					content: 'ARS설정 v1 / ARS설정 v2 / 회원가입 / 채팅 / 관리자', 
 				}, 
 			], 
 			corp: '서울이동통신', 
 			link: 'https://say015.com', 
 		}
 	}, 
+	
+	{
+		sliderProps: {
+			items: [
+				<img src={imgTmpGif} alt="" />, 
+				<img src={imgTmp} alt="" />, 
+				<div>3</div>, 
+			], 
+			slideWidth: slideWidth, 
+			slideHeight: slideHeight, 
+		}, 
+		projectDescProps: {
+			period: {
+				start: new Date('2020-09-01'), 
+				end: new Date('2021-02-01'), 
+			}, 
+			technologies: ['HTML5', 'Sass(SCSS)', 'ReactJS', 'ES6', 'Axios', 'Responsive Web'], 
+			details: [
+				{
+					title: '포지션', 
+					content: '웹 프론트엔드 개발자', 
+				}, 
+				{
+					title: '프로젝트', 
+					content: '수강후기 페이지 신설 및 반응형 도입 / 수강신청 유지보수', 
+				}, 
+				{
+					title: '담당기능', 
+					content: '다중 필터링 게시판 (페이징 직접 구현) / Youtube API사용 탭메뉴', 
+				}, 
+			], 
+			corp: '오누이', 
+			link: 'https://www.seoltab.com/seoltab_review', 
+		}
+	}, 
+
+	{
+		sliderProps: {
+			items: [
+				<img src={imgTmpGif} alt="" />, 
+				<img src={imgTmp} alt="" />, 
+				<div>3</div>, 
+			], 
+			slideWidth: slideWidth, 
+			slideHeight: slideHeight, 
+		}, 
+		projectDescProps: {
+			period: {
+				start: new Date('2013-03-01'), 
+				end: new Date('2018-11-01'), 
+			}, 
+			technologies: ['HTML5', 'CSS3', 'jQuery', 'VanillaJS', 'Cross Browsing', 'Accessibility'], 
+			details: [
+				{
+					title: '포지션', 
+					content: '웹 퍼블리셔', 
+				}, 
+				{
+					title: '프로젝트', 
+					content: '시원스쿨 일본어, 위런영어, b2b 사이트 오픈 / 시원스쿨 웹사이트 다수 리뉴얼 및 유지보수', 
+				}, 
+				{
+					title: '담당기능', 
+					content: '사이트 공통 기능(모달팝업, 탭메뉴, 슬라이더, 상하단 고정 배너 등) / fullPage, slimscroll, audio DOM 사용 애니메이션', 
+				}, 
+			], 
+			corp: '에스제이더블유인터내셔널', 
+			link: 'https://lab.siwonschool.com/', 
+			// link: 'https://www.siwonschool.com/?s=event&t=160318', 
+		}
+	}, 
+
 ]
--- src/views/main/MainTimeline.tsx
+++ src/views/main/MainTimeline.tsx
@@ -1,73 +1,124 @@
 import { forwardRef, useEffect, useRef, useState } from 'react'
 import { useWindowDimensions } from '../../utils/customHooks';
 
+interface IScrollInfo {
+	index: number, 
+	startYear: number, 
+	endYear: number, 
+	scrollTop: number, 
+	scrollBottom: number, 
+}
+
 export default forwardRef(function MainTimeline({startYear}: any, ref: any){
 	console.log('[MainTimeline]', ref);
-	const targetRefs = ref //timeline클릭시 이동할 element의 ref값들
-	const timelineRef: any = useRef(null)
+	const targetRefs = ref //timeline클릭시 이동할 element들. 순서보장(연도 내림차순)
 	const {height: windowH} = useWindowDimensions()
-	const scrollInfos: Array<any> = []
-	const clickInfos = new Map()
+	const timelineRef: any = useRef(null)
+	// let currScrollInfoIndex: any = null
+	const [currScrollInfo, setCurrScrollInfo] = useState<IScrollInfo|null>(null)
 	
+	// 클릭,스크롤 이벤트에 필요한 정보를 targetRefs에서 가져오기
+	const clickInfos = new Map()
+	const scrollInfos: Array<IScrollInfo> = []
 	targetRefs.current.forEach((ref: HTMLElement, i: number, refs: any) => {
-		const startYear = Number(ref.dataset.period!.split('-')[0])
-		const endYear = Number(ref.dataset.period!.split('-')[1])
 		const scrollTop = 
 			i === 0
-			? Math.abs(ref.getBoundingClientRect().top)
-			: Math.abs(ref.getBoundingClientRect().top) - windowH/2
+			? Math.round(ref.getBoundingClientRect().top + window.scrollY)
+			: Math.round(ref.getBoundingClientRect().top + window.scrollY) - windowH/2
 		const scrollBottom = 
 			i === refs.length-1
-			? Math.abs(ref.getBoundingClientRect().bottom)
-			: Math.abs(refs[i+1].getBoundingClientRect().top) - windowH/2
+			? Math.round(ref.getBoundingClientRect().bottom + window.scrollY)
+			: Math.round(refs[i+1].getBoundingClientRect().top + window.scrollY) - windowH/2
 
-		scrollInfos.push({
-			startYear, 
-			endYear, 
+		const startYear = Number(ref.dataset.period!.split('-')[0])
+		const endYear = Number(ref.dataset.period!.split('-')[1])
+		
+		const scrollInfo = {
+			index: i, 
 			scrollTop, 
 			scrollBottom, 
-		})
+			startYear, 
+			endYear, 
+		}
+
+		// 스크롤이벤트에 필요한 정보 세팅
+		scrollInfos.push(scrollInfo)
 		
-		// timeline클릭시 item별로 실행할 함수 세팅
+		// item별 클릭이벤트 세팅 (스크롤 이동 함수)
+		// 현 ref의 period범위에 속하는 item은 모두 동일한 함수 적용
 		Array.from({length: endYear-startYear+1}, (_, i)=>startYear+i).forEach((year, i) => {
-			// 
-			if (i===0 || !clickInfos.get(year)) {
-				clickInfos.set(year, ref.scrollIntoView)
+			if (!clickInfos.get(year) || i===0) {
+				clickInfos.set(year, ()=>{ref.scrollIntoView({behavior: 'smooth', block: 'center'})})
 			}
 		})
+
+		// 현 스크롤위치에 해당하는 정보를 상태값으로 업데이트 (처음 한번만)
+		if (
+			!currScrollInfo
+			&& (
+				(window.scrollY >= scrollTop && window.scrollY < scrollBottom)
+				|| (i===0 && window.scrollY < scrollTop)
+				|| (i===refs.length-1 && window.scrollY > scrollBottom)
+			)
+		) {
+			setCurrScrollInfo(scrollInfo)
+		}
 	})
 	
 	const renderTimelineItems = () => {
+		// console.log('window.scrollY:', window.scrollY, scrollInfos);
 		const currentYear = new Date().getFullYear()
 		
-		return Array.from({length: currentYear-startYear+1}, (_, i)=>currentYear-i).map((year, i) =>
-			<li key={year} data-year={year}
-				{...i===0 && {className: 'on'}}
-			>
-				{year === currentYear ? 'NOW' : year}
-			</li>
+		const getAttrs = (year: number) => {
+			const attrs: any = {}
+			// 현 스크롤위치에 따른 아이템 활성화
+			if (
+				currScrollInfo
+				&& year >= currScrollInfo.startYear
+				&& year <= currScrollInfo.endYear
+			) {
+				attrs.className = 'on'
+			}
+
+			// 클릭이벤트 등록
+			if (clickInfos.get(year)) {
+				attrs.onClick = clickInfos.get(year)
+			}else{
+				// clickInfos에 없는 연도는 비활성화
+				attrs.className = 'off'
+			}
+			
+			return attrs
+		}
+		
+		return Array.from({length: currentYear-startYear+1}, (_, i)=> {
+			const year = currentYear - i
+			return (
+				<li key={i}
+					data-year={year}
+					{...getAttrs(year)}
+				>
+					{i === 0 ? 'NOW' : year}
+				</li>
+			)
+		}
 		)
 	}
 
-	// {현 scrollTop값}이 {특정section 위치 - viewport높이값 1/3}에 오면
-	// ㄴ 그 section의 {period}에 해당하는 li에 class on
 	const onScroll = () => {
-		const scrollTop = window.scrollY;
-		const targetTop = targetRefs.current[0].getBoundingClientRect().top - windowH/2
-		const nextTargetTop = targetRefs.current[1].getBoundingClientRect().top - windowH/2
-		const period = {
-			start: targetRefs.current[0].dataset.period.split('-')[0], 
-			end: targetRefs.current[0].dataset.period.split('-')[1], 
-		}
-		if (scrollTop >= targetTop && scrollTop < nextTargetTop) {
-			Array.from(timelineRef.current!.children).forEach((item: any) => {
-				const itemDataYear = Number(item.dataset.year)
-				if (itemDataYear >= period.start && itemDataYear <= period.end) {
-					item.classList.add('on')
-				}else{
-					item.classList.remove('on')
-				}
-			})
+		if (
+			currScrollInfo
+			&& (
+				(window.scrollY < currScrollInfo.scrollTop && currScrollInfo.index !== 0)
+				|| (window.scrollY >= currScrollInfo.scrollBottom && currScrollInfo.index !== scrollInfos.length-1)
+			)
+		) {
+			// 현 스크롤위치가 설정범위를 벗어날 시 범위 재설정 (스크롤정보 업데이트)
+			const direction = window.scrollY - currScrollInfo.scrollTop < 0 ? -1 : 1
+			setCurrScrollInfo(scrollInfos[currScrollInfo.index + direction])
+
+			console.log(1, direction, window.scrollY, currScrollInfo.scrollTop, currScrollInfo.scrollBottom);
+			
 		}
 	};
 
@@ -78,14 +129,24 @@ export default forwardRef(function MainTimeline({startYear}: any, ref: any){
 		};
 	}, []);
 	
+	useEffect(()=>{
+		// 현 스크롤위치 범위에 해당하는 아이템 활성화
+		if (currScrollInfo) {
+			console.log(2, currScrollInfo.startYear, currScrollInfo.endYear);
+			Array.from(timelineRef.current!.children).forEach((item: any) => {
+				const itemDataYear = Number(item.dataset.year)
+				if (itemDataYear >= currScrollInfo.startYear && itemDataYear <= currScrollInfo.endYear) {
+					item.classList.add('on')
+				}else{
+					item.classList.remove('on')
+				}
+			})
+		}
+	}, [currScrollInfo])
+	
 	return (
 		<ul className='timeline' ref={timelineRef}>
 			{ renderTimelineItems() }
-			{/* <li data-year="2023" className='off'>NOW</li>
-			<li>2022</li>
-			<li>2021</li>
-			<li>2020</li>
-			<li className='off'>2019</li> */}
 		</ul>
 	)
 })
